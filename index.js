c9e29539de72b170858abdcbb68a49244758c2d8
Aarik Pokras <85502246+aarikpokras@users.noreply.github.com>
2024-01-29 20:19:43 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
-const bodyParser = require('body-parser');
+const bp = require('body-parser');
 const app = express();
 const fs = require('fs');
 
-app.use(bodyParser.json());
+app.use(bp.json());
 
 const hbs = exphbs.create({
   extname: 'hbs',
